fbaa4eafa08ebd3adc1a716954d8115420054af5
NguyenDuyViet <duyvietnguyen0805@gmail.com>
2024-10-14 19:30:03 +0700

    Da lam su kien cho signIn,SignUp va chinh lai layout

---
+++ app/src/main/java/com/example/travel_app/Activity/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.travel_app.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.travel_app.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
