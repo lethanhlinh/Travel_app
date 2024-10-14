4f8cbf61b52dd989b9cff4ec56857b471095b684
NguyenDuyViet <duyvietnguyen0805@gmail.com>
2024-10-14 19:59:14 +0700

    Da lam su kien cho SignInActivity.java va SignUpActivity.java doi mot so layout

--- app/src/main/java/com/example/travel_app/Activity/SignInActivity.java
+++ app/src/main/java/com/example/travel_app/Activity/SignInActivity.java
@@ -1,17 +1,37 @@
 package com.example.travel_app.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.travel_app.R;
+import com.example.travel_app.databinding.ActivityDetailBinding;
+import com.example.travel_app.databinding.ActivityIntroBinding;
+import com.example.travel_app.databinding.ActivitySigninBinding;
 
-public class ProfileActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
+    private ActivitySigninBinding binding;
+    //Khai báo user o day
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivitySigninBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //Dang nhap thanh cong chuyen huong den trang Intro
+        binding.btnSignIn.setOnClickListener(view -> {
+            Intent intent = new Intent(SignInActivity.this, IntroActivity.class);
+            //intent.putExtra("user", user);
+            startActivity(intent);
+        });
 
+        //Neu chua co tai khoan thi chuyen sang trang dang ky
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
